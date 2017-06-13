1fa247d1474b53032b27e80140e7b7021e5a911a
Sébastien LAVENANT <lavenant@s3b.fr>
2017-06-13 16:31:46 +0200

    fix: rabbitmq container up detection

--- rabbitmq.mk
+++ rabbitmq.mk
@@ -2,4 +2,7 @@ COMPOSER_VENDOR_DIR_PATH?=vendor/naoned/rabbitmq-docker
 
 up-rabbitmq:
 	$(info Ensure rabbitmq container is running)
-	@COMPOSE_PROJECT_NAME="naoned" docker-compose -f $(COMPOSER_VENDOR_DIR_PATH)/docker-compose.yml up -d
+	@if [ -z "$$(docker ps -a --filter name=rabbitmq --format {{.Names}} | grep -E '^rabbitmq$$')" ]; then \
+	    echo 'Creating rabbitmq container'; \
+	    COMPOSE_PROJECT_NAME="naoned" docker-compose -f $(COMPOSER_VENDOR_DIR_PATH)/docker-compose.yml up -d; \
+	fi
