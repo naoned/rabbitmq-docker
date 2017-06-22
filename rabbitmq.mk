05902dcbb5a966eb64ba241fd9dfd1b122423b4f
vcollignon <vcollignon@naoned.fr>
2017-06-22 15:28:07 +0200

    feat: update docker-compose for health check

--- rabbitmq.mk
+++ rabbitmq.mk
@@ -2,12 +2,10 @@ COMPOSER_VENDOR_DIR_PATH?=vendor/naoned/rabbitmq-docker
 
 up-rabbitmq:
 	$(info Ensure rabbitmq container is running)
-	@if [ -z "$$(docker ps --filter name=rabbitmq --format {{.Names}} | grep -E '^rabbitmq$$')" ]; then \
+	@if [ -z "$$(docker ps -a --filter name=rabbitmq --format {{.Names}} | grep -E '^rabbitmq$$')" ]; then \
 	    echo 'Creating rabbitmq container'; \
 	    COMPOSE_PROJECT_NAME="naoned" docker-compose -f $(COMPOSER_VENDOR_DIR_PATH)/docker-compose.yml up -d; \
 	fi
 
 wait-for-rabbitmq-to-be-ready:
-	@sh ${COMPOSER_VENDOR_DIR_PATH}/scripts/service-checker.sh rabbitmq
-
-.PHONY: wait-for-rabbitmq-to-be-ready
+	$(call wait-for-container-to-be-ready,rabbitmq,6,30)
--- scripts/healthcheck/rabbitmq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-if rabbitmqctl status; then
-    exit 0
-fi
-
-exit 1
