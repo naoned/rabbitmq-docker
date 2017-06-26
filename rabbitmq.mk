4f4ac39dc8738e3176544ea3c1b4a5abf986b2fe
Claude LE BRIS <claude.lebris@gmail.com>
2017-06-26 17:17:21 +0200

    fix: ensure container is really running

--- rabbitmq.mk
+++ rabbitmq.mk
@@ -2,7 +2,7 @@ COMPOSER_VENDOR_DIR_PATH?=vendor/naoned/rabbitmq-docker
 
 up-rabbitmq:
 	$(info Ensure rabbitmq container is running)
-	@if [ -z "$$(docker ps -a --filter name=rabbitmq --format {{.Names}} | grep -E '^rabbitmq$$')" ]; then \
+	@if [ -z "$$(docker ps --filter name=rabbitmq --format {{.Names}} | grep -E '^rabbitmq$$')" ]; then \
 	    echo 'Creating rabbitmq container'; \
 	    COMPOSE_PROJECT_NAME="naoned" docker-compose -f $(COMPOSER_VENDOR_DIR_PATH)/docker-compose.yml up -d; \
 	fi
